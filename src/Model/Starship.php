0d5f20a34c005235f70794b0281f4acf065f6e9d
Romain DURAND <146010665+Sonyoss@users.noreply.github.com>
2025-04-17 14:02:06 +0200

    Énumérations PHP

--- src/Model/Starship.php
+++ src/Model/Starship.php
@@ -8,7 +8,7 @@ class Starship{
         private string $name,
         private string $class,
         private string $captain,
-        private string $status,
+        private StarshipStatusEnum $status,
         ){}
 
     public function getId() : int
@@ -31,7 +31,7 @@ class Starship{
         return $this->captain;
     }
  
-    public function getStatus() : string
+    public function getStatus() : StarshipStatusEnum
     {
         return $this->status;
     }
